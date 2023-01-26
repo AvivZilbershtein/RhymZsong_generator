92409cfc20f13ac77b4ba6a7c4cce43e05f2b309
AvivZilbershtein <112809979+AvivZilbershtein@users.noreply.github.com>
2023-01-26 19:04:15 +0200

    global private api + comments

--- request.py
+++ request.py
@@ -1,6 +1,7 @@
 import os
 import openai
-
+# INSERT YOUR PRIVATE API KEY HERE
+PRIVATE_API_KEY = "WRITE_YOUR_PRIVATE_API_KEY"
 
 class Request:
     def __init__(self):
@@ -8,13 +9,15 @@ class Request:
         self.randomness = 0.5
         self.max_line = 6
         self.min_line = 3
-
+    
+    # in charge of updating the hyperparameters
     def change_preferences(self, line_repetition, randomness, max_line, min_line):
         self.line_repetition = line_repetition
         self.randomness = randomness
         self.max_line = max_line
         self.min_line = min_line
-
+    
+    # in charge of aggregating a prompt, give in to the GPT-3 to create a respond. return the response and the request
     def get_response(self, creation_type, size, genre, subject, context):
         request = "write a {} in the {} genre about a {} in the context of {} with only {} lines, where the max length of a line is {} words and the minimum length is {} words".format(
             creation_type, genre,  subject, context, size, self.max_line, self.min_line)
@@ -23,7 +26,7 @@ class Request:
             if type in subject:
                 request = request.replace(type, "")
         print(request)
-        openai.api_key = "WRITE_YOUR_PRIVATE_API_KEY"
+        openai.api_key = PRIVATE_API_KEY
         try:
             completion = openai.Completion.create(model="text-davinci-003",
                                                   prompt=request,
@@ -41,9 +44,9 @@ class Request:
             response = "Unfortunately your parameters weren't loaded properly. Try again"
         return response, request
 
-
+    # checks if the generated song's genre is correct. If not, a new response is created. returns this response
     def check_genre(self, response, genre):
-        openai.api_key = "WRITE_YOUR_PRIVATE_API_KEY"
+        openai.api_key = PRIVATE_API_KEY
         request = "what is the genre of the song you created? Write only the genre without a dot"
         print(request)
         completion = openai.Completion.create(model="text-davinci-003", prompt=request, max_tokens=4000,
@@ -62,6 +65,7 @@ class Request:
             response = completion.choices[0].text
         return response
 
+    # checks if the generated song's length is correct. If not, a new response is created. returns this response
     def check_length(self, response, size, subject, context,genre):
         if size != len(response):
             request = "redo the song to have exactly {} lines while keeping the subject {} in the context of {}, genre {}" \
@@ -74,7 +78,7 @@ class Request:
         return response
 
 
-
+# for local testing
 if __name__ == '__main__':
     request = Request()
     print(request.get_response("song", "20", "pop", "Ofra", "nature"))
